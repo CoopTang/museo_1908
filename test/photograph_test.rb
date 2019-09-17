0e4eab4d5069cb5520925ff2eeef94953d2064bf
Michael Cooper <macoop2363@gmail.com>
2019-09-17 09:17:46 -0600

    Added tests for existance and attributes

--- test/photograph_test.rb
+++ test/photograph_test.rb
@@ -5,11 +5,24 @@ require './lib/photograph'
 class PhotographTest < Minitest::Test
 
   def setup
-    @photograph = Photograph.new
+    @attributes = {
+      id: "1",
+      name: "Rue Mouffetard, Paris (Boy with Bottles)",
+      artist_id: "4",
+      year: "1954"
+    }   
+    @photograph = Photograph.new(@attributes)
   end
 
   def test_it_exists
     assert_instance_of Photograph, @photograph
+  end
+
+  def test_it_has_attributes
+    assert_equal @attributes[:id], @photograph.id
+    assert_equal @attributes[:name], @photograph.name
+    assert_equal @attributes[:artist_id], @photograph.artist_id
+    assert_equal @attributes[:year], @photograph.year
   end
 
 end
