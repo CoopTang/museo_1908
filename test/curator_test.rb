b7190195fc1a24eee26e3a970c03c97e6afc32c1
Michael Cooper <macoop2363@gmail.com>
2019-09-17 09:24:00 -0600

    Added new class and its respective test

---
+++ test/curator_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/artist'
+require './lib/photograph'
+require './lib/curator'
+
+class CuratorTest < Minitest::Test
+
+  def setup
+  end
+
+  def test_it_exists
+  end
+
+end
